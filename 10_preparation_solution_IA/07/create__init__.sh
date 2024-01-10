f37b3069940401a3e05b2a8c171eeeddc39074ca
Renato <rennyatwork@gmail.com>
2024-01-09 22:03:42 -0500

    07 - tox worked

---
+++ 10_preparation_solution_IA/07/create__init__.sh
@@ -0,0 +1,3 @@
+touch ./packages/regression_model/regression_model/__init__.py
+touch ./packages/regression_model/regression_model/config/__init__.py
+touch ./packages/regression_model/regression_model/processing/__init__.py
